37d6372d3f899fab0f1daa38da1c0eb8498f1c33
Rishabh Pandey <rishabhpandey2963@gmail.com>
2025-10-02 16:05:42 +0530

    payment gateway removed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 // process.env.ATLAS_URL
 
 try{
-    await mongoose.connect(process.env.ATLAS_URL)
+    await mongoose.connect("mongodb://localhost:27017/holidayApp")
 }catch(err){
     // console.log(err);
     console.log("error connecting to db");
